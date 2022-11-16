d3c971c714c73d7d46262abd7cb9fb5d9476cf23
Jeferson X <jefersonx0140@gmail.com>
2022-11-16 22:15:55 +0530

    DriverWait

--- Comic.py
+++ Comic.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 from webdriver_manager.chrome import ChromeDriverManager
 
 import mysql.connector
-import time
 import Email
 
 # Establish connection with mySQL database
@@ -33,22 +34,30 @@ def manga_plus():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
     driver.get(url)
     print("refreshing webpage...")
-    # Wait for webpage to load
-    time.sleep(3)
 
-    # Get titles of comics with new chapters from the website
-    new_titles = driver.find_elements(By.CLASS_NAME, "LastUpdatedTitle-module_allTitle_20kmL")
-
-    for titles in new_titles[0:10]:
-        title = titles.find_element(By.CLASS_NAME, "LastUpdatedTitle-module_title_3HJEY")
-        for comic in comic_list:
-            if title.text == comic[1]:
-                print("found title match!")
-                # getting image and link of the comic
-                link = titles.get_attribute("href")
-                img = titles.find_element(By.CLASS_NAME, "LastUpdatedTitle-module_image_33lsO")
-                src = img.get_attribute("src")
-                last_read(link, comic, src)
+    try:
+        # Get titles of comics with new chapters from the website
+        new_titles = WebDriverWait(driver, 5).until(
+            ec.presence_of_all_elements_located((By.CLASS_NAME, "LastUpdatedTitle-module_allTitle_20kmL"))
+        )
+        for titles in new_titles[0:5]:
+            # title = WebDriverWait(titles, 2).until(
+            #    ec.presence_of_element_located((By.CLASS_NAME, "LastUpdatedTitle-module_title_3HJEY")))
+            title = titles.find_element(By.CLASS_NAME, "LastUpdatedTitle-module_title_3HJEY")
+            # print(title.text)
+            for comic in comic_list:
+                if title.text == comic[1]:
+                    print("found title match!")
+                    # getting image and link of the comic
+                    link = titles.get_attribute("href")
+                    img = WebDriverWait(titles, 10).until(
+                        ec.visibility_of_element_located((By.CSS_SELECTOR, "img[src]")))
+                    # img = titles.find_element(By.CLASS_NAME, "LastUpdatedTitle-module_image_33lsO")
+                    src = img.get_attribute("src")
+                    print(src)
+                    last_read(link, comic, src)
+    finally:
+        driver.quit()
 
 
 def last_read(link, comic, src):
@@ -56,26 +65,30 @@ def last_read(link, comic, src):
     driver.get(link)
 
     print("loading the comic page...")
-    time.sleep(3)
-    # get list of new chapters in comic
-    new_chapters = driver.find_elements(By.CLASS_NAME, "ChapterListItem-module_title_3Id89")
-    recent_chapter = new_chapters[-1]
+    try:
+        # get list of new chapters in comic
+        new_chapters = WebDriverWait(driver, 5).until(
+            ec.presence_of_all_elements_located((By.CLASS_NAME, "ChapterListItem-module_title_3Id89"))
+        )
+        recent_chapter = new_chapters[-1]
 
-    # check if the notification for the new chapter has already been sent
-    if recent_chapter.text != comic[2]:
-        print("sending mail...")
-        my_cursor.execute("SELECT email_id FROM emails JOIN users ON emails.user_id = users.user_id WHERE "
-                          "comic_id = " + str(comic[0]))
-        emails = my_cursor.fetchall()
+        # check if the notification for the new chapter has already been sent
+        if recent_chapter.text != comic[2]:
+            print("sending mail...")
+            my_cursor.execute("SELECT email_id FROM emails JOIN users ON emails.user_id = users.user_id WHERE "
+                              "comic_id = " + str(comic[0]))
+            emails = my_cursor.fetchall()
 
-        my_cursor.execute("UPDATE comics SET last_chapter = '" + recent_chapter.text +
-                          "' WHERE comic_id = " + str(comic[0]))
-        mydb.commit()
+            my_cursor.execute("UPDATE comics SET last_chapter = '" + recent_chapter.text +
+                              "' WHERE comic_id = " + str(comic[0]))
+            mydb.commit()
 
-        print(recent_chapter.text)
-        Email.mail(comic[1], recent_chapter.text, link, emails, src)
-    else:
-        print("already read")
+            print(recent_chapter.text)
+            Email.comic_mail(comic[1], recent_chapter.text, link, emails, src)
+        else:
+            print("already read")
+    finally:
+        driver.quit()
 
 
 manga_plus()
--- Email.py
+++ Email.py
@@ -3,12 +3,11 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+sender_email = "1nh20cs096.jefersonx@gmail.com"
+password = "qumaxmjtdkczyvgp"
 
-def mail(title, chapter, link, email_ids, src):
-    sender_email = "1nh20cs096.jefersonx@gmail.com"
-    password = "wsmjrjwdxzajjwfi"
-    subject = title
 
+def comic_mail(title, chapter, link, email_ids, src):
     html = """\
             <!DOCTYPE html>
             <html>
@@ -20,19 +19,43 @@ def mail(title, chapter, link, email_ids, src):
                             New chapter is out now on <b>MANGA Plus</b><br />
                             Click to read now.
                         </p>
-                    </div>
-                    <div>
                         <center>
-                            <a href=""" + link + """><img src=""" + src + """/></a> <p> Read the latest chapters from our 
-                            weekly magazine for free and simultaneously with Japan.<br /> Download MANGA Plus now and get 
-                            started! </p> <a href="https://play.google.com/store/apps/details?id=jp.co.shueisha.mangaplus
-                            "><img src="https://mangaplus.shueisha.co.jp/img/web_logo_190118_light.c773f9e9.png"/></a> 
+                            <a href=""" + link + """><img src=""" + src + """/></a> <p> Read the latest chapters from 
+                            our weekly magazine for free and simultaneously with Japan.<br /> Download MANGA Plus now 
+                            and get started! </p> <a 
+                            href="https://play.google.com/store/apps/details?id=jp.co.shueisha.mangaplus "><img 
+                            src="https://mangaplus.shueisha.co.jp/img/web_logo_190118_light.c773f9e9.png"/></a> 
                             <p>©︎2019 Shueisha Inc. All rights reserved. <br /></p>
                         </center>
                     </div>
                 </body>
             </html> """
+    send_mail(html, email_ids, title)
 
+
+def news_mail(title, link, src, day, month, email_ids):
+    html = """\
+                <!DOCTYPE html>
+                <html>
+                    <body>
+                        <div>
+                            <h1>""" + title + """</h1>
+                            <h2>""" + link + """</h2>
+                            <p>
+                                New upcoming event in <b>New Horizon College of Engineering</b><br />
+                                Date: <br />""" + day + month + """<br />
+                            </p>
+                            <center>
+                                <a href=""" + link + """><img src=""" + src + """/></a>
+                                <p>© New Horizon Educational Institution<br /></p>
+                            </center>
+                        </div>
+                    </body>
+                </html> """
+    send_mail(html, email_ids, "New Horizon Events")
+
+
+def send_mail(html, email_ids, subject):
     for receiver_email in email_ids:
         message = MIMEMultipart()
         message["From"] = sender_email
